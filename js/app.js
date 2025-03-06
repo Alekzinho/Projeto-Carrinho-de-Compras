e49f9b1507d0172d5db2d598cd5df0189c6b10c2
Alex Olmedo <aricardoolopes@gmail.com>
2025-03-06 20:15:52 -0300

    Produto válido e quantidade válida

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 function adicionar() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     
-    if (isNaN(quantidade)) {
-        quantidade = 1;
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
     }
 
     let produto = document.getElementById("produto").value;
     let nomeProduto = produto.split("-")[0];
+
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
     
     let valor = parseInt(document.getElementById("valor-total").innerHTML.split("R$")[1]);
     let valorProduto = parseInt(produto.split("R$")[1]);
@@ -15,12 +21,13 @@ function adicionar() {
     let carrinho = document.getElementById("lista-produtos");
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
     <section class="carrinho__produtos__produto">
-      <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+      <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorTotalProduto}</span>
     </section>`;
     
     let valorFinal = valor + valorTotalProduto;
     let resultadoFinal = document.getElementById("valor-total").innerHTML = `R$${valorFinal}`; 
 
+    document.getElementById("quantidade").value = "";
 }
 
 function limpar() {
