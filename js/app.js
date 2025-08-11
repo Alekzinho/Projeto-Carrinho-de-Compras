22ae0fb40faed137c9ead377fe4e148b1ce91ff9
Alex Olmedo <aricardoolopes@gmail.com>
2025-08-11 03:18:18 -0300

    Atividade 4: Soma de dois números

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ function enviar() {
     let cidade = document.getElementById("cidade").value;
 
     let novaMensagem = document.getElementById("mensagem");
-    novaMensagem.textContent = `Espero que você tenha se divertido ${nome}, e que nesses ${idade} anos de vida você tenha realizado seus objetivos, sem dúvida seus amigos de ${cidade} tem orgulho de você.`
+    novaMensagem.textContent = `Espero que você tenha se divertido ${nome}, e que nesses ${idade} anos de vida você tenha realizado seus objetivos, sem dúvida seus amigos de ${cidade} sentem orgulho de você.`
     console.log(nome, idade, cidade);
-    ;
 }
+
+let numero1 = 45;
+let numero2 = 55;
+let resultado = numero1 + numero2;
+
+console.log(`Você sabia que a soma de ${numero1} com ${numero2} é igual a ${resultado}?`);
