16a3bdf062b81777efd60d5b5d3ba088c7f07b00
Alex Olmedo <aricardoolopes@gmail.com>
2025-08-11 03:05:15 -0300

    Atividade 3: Mudando o parágrafo com JS

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ function enviar() {
     let idade = document.getElementById("idade").value;
     let cidade = document.getElementById("cidade").value;
 
+    let novaMensagem = document.getElementById("mensagem");
+    novaMensagem.textContent = `Espero que você tenha se divertido ${nome}, e que nesses ${idade} anos de vida você tenha realizado seus objetivos, sem dúvida seus amigos de ${cidade} tem orgulho de você.`
     console.log(nome, idade, cidade);
-    alert("Olá " + nome + ", entâo você tem " + idade + " anos e mora em " + cidade + ".");
-
+    ;
 }
